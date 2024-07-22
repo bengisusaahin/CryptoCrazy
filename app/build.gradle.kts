d12e21bdee105275717b470cb6e1fdf3fc66c1d8
Bengisu Şahin <74653216+bengisusaahin@users.noreply.github.com>
2024-07-22 22:07:38 +0300

    feat: added dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,6 +73,36 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 
+    implementation("androidx.compose.material:material-icons-core:1.6.8")
+    implementation("androidx.compose.material:material-icons-extended:1.6.8")
+    implementation("androidx.constraintlayout:constraintlayout-compose:1.1.0-alpha09")
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
+    implementation("androidx.navigation:navigation-compose:2.6.0-rc01")
+    implementation("com.google.accompanist:accompanist-flowlayout:0.17.0")
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.2")
+    implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.2")
+
+    // Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+
+    // Coroutine Lifecycle Scopes
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+
+    // Coil
+    implementation("io.coil-kt:coil-compose:1.3.2")
+
+    //Dagger - Hilt
     implementation(libs.hilt.android)
     kapt(libs.hilt.android.compiler)
+    kapt("androidx.hilt:hilt-compiler:1.0.0")
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
+    implementation("androidx.hilt:hilt-work:1.0.0")
+    implementation("androidx.work:work-runtime-ktx:2.8.1")
 }
---
+++ app/src/main/java/com/bengisusahin/cryptocrazy/util/Constants.kt
@@ -0,0 +1,14 @@
+package com.bengisusahin.cryptocrazy.util
+
+object Constants {
+    // https://api.nomics.com/v1/prices?key=%3CKendi%20API%C2%A0Anahtar%C4%B1n%C4%B1z%3E
+
+    const val BASE_URL = "https://api.nomics.com/v1/"
+    const val API_KEY = ""
+    const val CALL_ATTRIBUTES = "id,name,logo_url"
+
+
+    // https://api.nomics.com/v1/currencies?key=%3CKendi%20API%C2%A0Anahtar%C4%B1n%C4%B1z%3E&ids=BTC&attributes=id,name,logo_url
+    // https://raw.githubusercontent.com/atilsamancioglu/IA32-CryptoComposeData/main/cryptolist.json
+    // https://raw.githubusercontent.com/atilsamancioglu/IA32-CryptoComposeData/main/crypto.json
+}
